5e4215923dfae9b5aa9e826e059a172f8b3f93f4
Aditya <adirocks1907@gmail.com>
2021-07-29 17:22:56 +0530

    Added book cover code

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
+
+import json
+from isbntools.app import *
+import requests
+
+from urllib.request import urlopen
+
 app = Flask(__name__)
 model = load_model("model.h5")
 cv_saved = cv = pickle.load(open('cv.pkl', 'rb'))
@@ -27,44 +34,24 @@ def comeback():
 def upload():
     if request.method == 'POST':
 
-        # print(request.form["comment"])
         comment_text = request.form["comment"]
         book_name = request.form["book_name"]
-        print(book_name, comment_text)
-        # print(model.predict(cv.transform(['i did not like this book. Not good. Poor . Waste of time'])))
-        # print(model.predict(cv.transform([comment_text])))
+
+        api = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+
+        ISBN = isbn_from_words(book_name)
+
+        resp = urlopen(api + ISBN)
+        book_data = json.load(resp)
+
+
+# print(get_isbn)
+        image_link = book_data['items'][0]['volumeInfo']['imageLinks']['thumbnail']
+
         prediction = model.predict(cv.transform([comment_text]))
         if round(prediction[0][0]) == 0:
-            #     print("positive review!")
-            # else:
-            #     print("negative review!")
-            return render_template("positive_result.html")
-
-        return render_template("negative_result.html")
-    #     # f = request.files['image']
-    #     # print("current path")
-    #     # basepath = os.path.dirname(__file__)
-    #     # print("current path", basepath)
-    #     # filepath = os.path.join(basepath, 'uploads', f.filename)
-    #     # print("upload folder is ", filepath)
-    #     # f.save(filepath)
-
-    #     # img = image.load_img(filepath, target_size=(64, 64))
-    #     # x = image.img_to_array(img)
-    #     # x = np.expand_dims(x, axis=0)
-    #     # x = "Great book loved it!"
-    #     f = request.files['comment']
-    #     print(f)
-    #     preds = model.predict(x)
-
-    #     print("prediction", preds)
-
-    #     index = ['Bear', 'Crow', 'Elephant', 'Racoon', 'Rat']
-
-    #     print(np.argmax(preds))
-
-    #     text = "The animal is a.... : " + str(index[np.argmax(preds)])+" !"
-    # return text
+            return render_template("positive_result.html", link=image_link)
+        return render_template("negative_result.html", link=image_link)
 
 
 if __name__ == '__main__':
